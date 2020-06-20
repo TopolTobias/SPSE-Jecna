089ab8cee8bf004eca40ad46fb63689b27a5a432
Michal Vorlicek <michal.vorlicek.cz@seznam.cz>
2020-06-20 11:57:33 +0200

    Code correction

--- CtverceAObdelniky/src/Ctverce.java
+++ CtverceAObdelniky/src/Ctverce.java
@@ -1,22 +1,38 @@
 
 public class Ctverce {
-private int strana;
-	
-	public void setStrana(int str){
-		this.strana = str;
+	private int strana;
+	private Integer integer;
+
+//	Set nic nekontroluje
+	/*
+	 * public void setStrana(int str){ this.strana = str; }
+	 */
+	public void setStrana(int strana) {
+		if (strana < 0) {
+			this.strana = Math.abs(strana);
+		} else if (strana > 0) {
+			this.strana = strana;
+		} else {
+			integer = (Integer) null;
+			this.strana = integer;
+		}
 	}
-	public int getStrana(){
-	     return this.strana;
+
+	public int getStrana() {
+		return this.strana;
 	}
-	public Ctverce(){
-	     this.strana = 1;
+
+	public Ctverce() {
+		this.strana = 1;
 	}
-	public Ctverce(int str){
-	     this.strana = str;
+
+	// Konstruktor nebere hodnoty ze Setu
+	public Ctverce(int strana) {
+		setStrana(strana);
 	}
-	
-	public String toString(){
-	     return "Ctverec o strane " + this.strana;
+
+	public String toString() {
+		return "Ctverec o strane " + this.strana;
 	}
 
 }
